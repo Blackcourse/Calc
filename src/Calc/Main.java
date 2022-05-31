4b8bf6523642422c8a3e738d98002420ec6e47d2
Blackcourse <66360396+Blackcourse@users.noreply.github.com>
2022-05-31 09:35:44 +0300

    change

--- src/Calc/Main.java
+++ src/Calc/Main.java
@@ -1,13 +1,29 @@
 package Calc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Input 2 values");
         Scanner console = new Scanner(System.in);
         int num1 = console.nextInt();
         int num2 = console.nextInt();
-        int resul = num1/num2;
-        System.out.println(resul);
+        System.out.println("Input operation +, -, *, /");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String op1 = reader.readLine();
+        switch (op1) {
+            case "+":
+                System.out.println(num1 + num2);
+            case "-":
+                System.out.println(num1 - num2);
+            case "*":
+                System.out.println(num1 * num2);
+            case "/":
+                System.out.println(num1 /= num2);
+        }
+
     }
 }
